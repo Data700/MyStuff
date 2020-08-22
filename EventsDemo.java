8980f44c761240d4c04d482f1168683d976b4272
Data700 <data790@gmail.com>
2020-08-21 21:58:43 -0400

    Add files via upload

---
+++ EventsDemo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Steve
+ */
+public class EventsDemo {
+    {
+		public static void main(String[] args)
+			{
+				Event test001 = new Event();
+				test001.setEventNumber();
+				test001.setGuests();
+				CarlysEventPriceWithMethods.displayMotto();
+				CarlysEventPriceWithMethods.displayEvent(Integer.parseInt(test001.getEventNumber(test001.getGuests(test001.isLargeEvent(test001.getPrice())))));
+			}
+	}
